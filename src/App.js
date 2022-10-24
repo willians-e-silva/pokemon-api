c121c65efbe48a945ce6a629c569c1191e03113e
Mr.Brightside <willians.silva9620@gmail.com>
2022-10-24 03:48:28 -0300

    created the structure for frontend [pokedex and background implemented]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import './styles/main.css';
+
+import backgroundImage from './assets/background.png'
+import pokedex from './assets/pokedex.png'
+
+// import background from './assets/background.png'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <img src={background} alt="" className='image' />
+//     </div>
+//   );
+// }
+
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-[100vw] h-[100vh] bg-no-repeat bg-cover bg-grass' style={{ backgroundImage: `url(${backgroundImage})`}}>
+      <img src={pokedex} alt="" className='my-auto mx-auto h-[90vh] sticky-top-0 pt-20' />
     </div>
   );
-}
-
-export default App;
+};
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.js',
+    './index.html'
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
